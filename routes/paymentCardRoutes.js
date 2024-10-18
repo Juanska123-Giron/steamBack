6b48d0f51d21e8b6a1e2108c3a182687dddac82f
FernandoSilva02 <fernandosilvag0212@gmail.com>
2024-10-17 19:25:49 -0500

    Ajustar medios de pago

--- routes/paymentCardRoutes.js
+++ routes/paymentCardRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { deleteCard, newCard } from "../controllers/paymentCardController.js";
+import { deleteCard, newCard, chargeUser } from "../controllers/paymentCardController.js";
 import checkAuth from "../middleware/checkAuth.js";
 router.post("/", checkAuth, newCard); //Creating a new card
 router.delete("/delete/:id", deleteCard); //Deleting a card;
+router.post("/charge", chargeUser); //Charging a user
 
 export default router;

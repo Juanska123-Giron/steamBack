3fe5d349b7917a2e570443044b1ca92dc67e2eb5
FernandoSilva02 <fernandosilvag0212@gmail.com>
2024-10-30 21:11:12 -0500

    Remove unused route for fetching all categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,6 +5,5 @@ import checkAuth from "../middleware/checkAuth.js";
 router.post("/create", checkAuth, newCategory); //Creating a new category
 router.get("/", checkAuth, allCategory); //Getting all categories
 router.delete("/delete/:id", checkAuth, deleteCategory); //Deleting a category
-router.get("/getAllCategories", checkAuth, fetchCategories);
 
 export default router;
